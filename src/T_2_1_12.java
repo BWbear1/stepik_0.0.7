b1262d8970a274faaad174f7064fccd6a3c9dd97
BWbear1 <blackwhitebear1@gmail.com>
2023-08-19 14:53:53 +0200

    fix

--- src/T_2_1_12.java
+++ src/T_2_1_12.java
@@ -1,5 +1,9 @@
-public class Main {
+public class T_2_1_12 {
+    public static void main(String[] args) {
+    }
+
     public static boolean doubleExpression(double a, double b, double c) {
+        return Math.abs(c - a - b) < 0.0001;
     }
 }
-        return  Math.abs(c-a-b) < 0.0001;
+
