66688f5450717f7baa0d0a88b28f93ec59f2253d
BWbear1 <blackwhitebear1@gmail.com>
2023-08-19 13:51:35 +0200

    2_1_8 v1.0 https://stepik.org/lesson/12759/step/8?unit=3107

--- src/T_2_1_6.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static boolean main(String[] args) {
-        public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d){
-            return (a && b && !c && !d || a && !b && c && !d || !a && b && c && !d || !a && b && !c && d
-                    || a && !b && !c && d || !a && !b && c && d)
-
-        }
