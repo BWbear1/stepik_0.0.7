c1f82d2fef7142d1c0d96e5ba5fb02b6f99e3f9f
BWbear1 <115574532+BWbear1@users.noreply.github.com>
2023-08-19 14:55:34 +0200

    T 2 1 12 (#3)
    * 2_1_8 v1.0 https://stepik.org/lesson/12759/step/8?unit=3107
    
    * 2_1_12 v1.0 https://stepik.org/lesson/12759/step/12?unit=3107
    
    * 2_1_12 v1.0 https://stepik.org/lesson/12759/step/12?unit=3107
    
    * 2_1_12 v1.0 https://stepik.org/lesson/12759/step/12?unit=3107
    
    * fix

---
+++ src/T_2_1_12.java
@@ -0,0 +1,9 @@
+public class T_2_1_12 {
+    public static void main(String[] args) {
+    }
+
+    public static boolean doubleExpression(double a, double b, double c) {
+        return Math.abs(c - a - b) < 0.0001;
+    }
+}
+
