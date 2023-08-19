daa2940576f15cd69cff5ced84917e7a1530088f
BWbear1 <blackwhitebear1@gmail.com>
2023-08-19 14:45:23 +0200

    fix

--- src/T_2_1_6.java
+++ src/T_2_1_6.java
@@ -1,9 +1,10 @@
-public class Main {
-    public static boolean main(String[] args) {
+public class T_2_1_6 {
+    public static void main(String[] args) {
     }
-}
-    public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d){
+
+    public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
         return (a && b && !c && !d || a && !b && c && !d || !a && b && c && !d || !a && b && !c && d
                 || a && !b && !c && d || !a && !b && c && d);
 
     }
+}
